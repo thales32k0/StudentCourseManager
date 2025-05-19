acfe53b5ee4c1df828f0f15d585d6fe076ae3d23
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2025-05-19 15:27:29 -0300

    Update StudentCourseManagerApplication.java

--- src/main/java/com/meuprojeto/studentcoursemanager/StudentCourseManagerApplication.java
+++ src/main/java/com/meuprojeto/studentcoursemanager/StudentCourseManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentCourseManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentcoursemanagerApplication.class, args);
+		SpringApplication.run(StudentCourseManagerApplication.class, args);
 	}
 
 }
